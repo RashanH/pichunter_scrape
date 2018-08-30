d40ef2f408627ff4ad996c0d4a1239024b0d3386
RashanH <codexlanka000@gmail.com>
2018-08-30 13:23:18 +0530

    fixed startup album dir

--- main.py
+++ main.py
@@ -14,26 +14,25 @@ last_album = last_albumz[0]
 
 #send http requests to download images
 def album_scrape(id):
-    #try:
+    try:
         path = pics_directory + "/" + str(id)
         if not os.path.exists(path):
         os.mkdir(path)
         urllib.request.urlretrieve("https://y2.pichunter.com/" + str(id) + "_1_o.jpg", pics_directory + "/" + str(id) + "/" + str(id) + "_1_o.jpg")
         complete_album(id)
-    #except:
-        #path = pics_directory + "/" + str(id)
-        #if os.path.exists(path) and os.path.isdir(path):
-            #shutil.rmtree(path)
-        #print("This is an error message! - " + str(id))
+    except:
+        path = pics_directory + "/" + str(id)
+        if os.path.exists(path) and os.path.isdir(path):
+            shutil.rmtree(path)
+        print("This is an error message! - " + str(id))
 
 #download whole album
 def complete_album(id):
     for item in range(2, 31):
-        #try:
+        try:
             urllib.request.urlretrieve("https://y2.pichunter.com/" + str(id) + "_" + str(item) + "_o.jpg", pics_directory + "/" + str(id) + "/" + str(id) + "_" + str(item) + "_o.jpg")
-            #return
-        #except:
-            #return
+        except:
+            return
 
 #loop albums
 for x in range(albums_for_once):
